5134bfe2a7e8af531eeaa81372c10960d91c0693
Patrik Oravecz <acm9597@gmail.com>
2020-10-11 18:18:11 +0200

    asd

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,6 +34,22 @@ namespace UserMaintenance
             users.Add(u);
         }
 
-        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(sfd.FileName))
+                {
+                    foreach (var u in users)
+                    {
+                        sw.Write(u.ID);
+                        sw.Write(";");
+                        sw.Write(u.FullName);
+                        sw.WriteLine();
+                    }
+                }
+            }
+        }
     }
 }
