a1fc34467178244cd9729091e4316d06c149286e
Patrik Oravecz <acm9597@gmail.com>
2020-10-18 20:20:45 +0200

    commit2

--- gyak6/gyak6/Form1.cs
+++ gyak6/gyak6/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak6.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,32 @@ namespace gyak6
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
+            
             InitializeComponent();
+            // A változó deklarációk jobb oldalán a "var" egy dinamikus változó típus.
+            // A "var" változó az első értékadás pillanatában a kapott érték típusát veszi fel, és később nem változtatható.
+            // Jelen példa első sora tehát ekvivalens azzal, ha a "var" helyélre a MNBArfolyamServiceSoapClient-t írjuk.
+            // Ebben a formában azonban olvashatóbb a kód, és változtatás esetén elég egy helyen átírni az osztály típusát.
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            // Ebben az esetben a "var" a GetExchangeRates visszatérési értékéből kapja a típusát.
+            // Ezért a response változó valójában GetExchangeRatesResponseBody típusú.
+            var response = mnbService.GetExchangeRates(request);
+
+            // Ebben az esetben a "var" a GetExchangeRatesResult property alapján kapja a típusát.
+            // Ezért a result változó valójában string típusú.
+            var result = response.GetExchangeRatesResult;
+
         }
     }
 }
