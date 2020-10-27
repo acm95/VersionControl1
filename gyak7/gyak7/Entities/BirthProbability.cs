6b1343c376536e1ad829719c81292fc1be676338
Patrik Oravecz <acm9597@gmail.com>
2020-10-27 17:56:18 +0100

    feladat3

---
+++ gyak7/gyak7/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak7.Entities
+{
+    public class BirthProbability
+    {
+        public int BAge { get; set; }
+        public int ChildNumber { get; set; }
+        public double BPossibility { get; set; }
+    }
+}
---
+++ gyak7/gyak7/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak7.Entities
+{
+    public class DeathProbability
+    {
+        public int Sex { get; set; }
+        public int DAge { get; set; }
+        public double DPossibility { get; set; }
+    }
+}
