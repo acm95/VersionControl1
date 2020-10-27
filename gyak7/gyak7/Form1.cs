64b5f86532ed8f04f2c518ac62af66164f76699f
Patrik Oravecz <acm9597@gmail.com>
2020-10-27 18:10:21 +0100

    feladat6

--- gyak7/gyak7/Form1.cs
+++ gyak7/gyak7/Form1.cs
@@ -89,5 +89,27 @@ namespace gyak7
 
             return deathProbabilities;
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Végigmegyünk a vizsgált éveken
+            for (int year = 2005; year <= 2024; year++)
+            {
+                // Végigmegyünk az összes személyen
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    // Ide jön a szimulációs lépés
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
+        }
     }
 }
