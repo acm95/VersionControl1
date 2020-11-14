fe01362ef23dd1af51255405d0bda7b81728ed6a
Patrik Oravecz <acm9597@gmail.com>
2020-11-14 18:27:46 +0100

    commit9_2

---
+++ 8gyak/8gyak/Entities/CarFactory.cs
@@ -0,0 +1,17 @@
+﻿using _8gyak.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8gyak.Entities
+{
+    public class CarFactory : IToyFactory
+    {
+        public Toy CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
