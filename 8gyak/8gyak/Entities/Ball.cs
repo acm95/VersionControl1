1fe9c45deb93987fb54f75add6c46fbc6adf4000
Patrik Oravecz <acm9597@gmail.com>
2020-11-14 18:48:14 +0100

    commit13

--- 8gyak/8gyak/Entities/Ball.cs
+++ 8gyak/8gyak/Entities/Ball.cs
@@ -11,7 +11,12 @@ namespace _8gyak.Entities
 {
     public class Ball : Toy
     {
-        
+        public SolidBrush BallColor { get; private set; }
+
+        public Ball(Color color)
+        {
+            BallColor = new SolidBrush(color);
+        }
 
         protected override void DrawImage(Graphics g)
         {
